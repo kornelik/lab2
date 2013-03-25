8a94c04bcc2fc796dd046ef12486836c0bad1156
user <user@ubuntu.(none)>
2013-03-25 14:54:10 -0400

    cut between words Temperature and Dew

--- geo.sh
+++ geo.sh
@@ -2,3 +2,4 @@
 wget -q -O weather.dump http://www.weathercity.com/by/minsk
 sed 's/<[^<>]*>//g' weather.dump > weather.txt
 sed ':a;N;$!ba;s/\n/@/g' weather.txt > weather.line
+grep -w -Po 'Temperature:(.*)C@Dew' weather.line > first.line
