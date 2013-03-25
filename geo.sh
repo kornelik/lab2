612eafb8ab6668aadf8c761d617cf071020bc275
user <user@ubuntu.(none)>
2013-03-25 15:01:52 -0400

    output temperature

--- geo.sh
+++ geo.sh
@@ -3,3 +3,4 @@ wget -q -O weather.dump http://www.weathercity.com/by/minsk
 sed 's/<[^<>]*>//g' weather.dump > weather.txt
 sed ':a;N;$!ba;s/\n/@/g' weather.txt > weather.line
 grep -w -Po 'Temperature:(.*)C@Dew' weather.line > first.line
+grep -Po '(\-|\+){0,1}\d+' first.line > second.line
