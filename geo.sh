dcd2438606f5ccce9667dfbd22921142936d9817
user <user@ubuntu.(none)>
2013-03-25 16:16:23 -0400

    added connection and check it

--- geo.sh
+++ geo.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-wget -q -O weather.dump http://www.weathercity.com/by/minsk
-sed 's/<[^<>]*>//g' weather.dump > weather.txt
-sed ':a;N;$!ba;s/\n/@/g' weather.txt > weather.line
-grep -w -Po 'Temperature:(.*)C@Dew' weather.line > first.line
-grep -Po '(\-|\+){0,1}\d+' first.line > second.line
+
+connection()
+{
+	return 1
+}
+
+TIME=$(awk -F "=" '/TIME/{print $2}' geo.ini)
+
+while true 
+do 
+	if [ connection ];
+	then
+		wget -q -O weather.dump http://www.weathercity.com/by/minsk
+		sed 's/<[^<>]*>//g' weather.dump > weather.txt
+		sed ':a;N;$!ba;s/\n/@/g' weather.txt > weather.line
+		grep -w -Po 'Temperature:(.*)C@Dew' weather.line > first.line
+		grep -Po '(\-|\+){0,1}\d+' first.line > second.line
+		cat second.line
+		sleep $TIME
+	
+	else
+		echo "no connection"
+	fi
+done
